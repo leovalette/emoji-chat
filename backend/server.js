a4bb9a368e85a7e9784a2726c39e1703da3f368e
Léo Valette <leo.valette@viseo.com>
2023-11-08 11:48:38 +0100

    Add author and authorColor to messages

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,35 @@ const server = new WebSocket.Server({ port: 8080 });
 server.on("connection", (socket) => {
   console.log("Connection established");
 
+  const authors = [
+    { name: "LVA", color: "#FF00FF" },
+    { name: "Léo", color: "#0000FF" },
+    { name: "Olé", color: "#FF0000" },
+    { name: "Definitly not Léo", color: "#C0C0C0" },
+    { name: "Oél", color: "#00FFFF" },
+  ];
+
   const sendRandomEmoji = () => {
-    const randomEmoji = emoji.random().emoji;
-    socket.send(randomEmoji);
+    const authorIndex = Math.floor(Math.random() * authors.length);
+    socket.send(
+      JSON.stringify({
+        message:
+          emoji.random().emoji +
+          " " +
+          emoji.random().emoji +
+          " " +
+          emoji.random().emoji +
+          " " +
+          emoji.random().emoji +
+          " " +
+          emoji.random().emoji,
+        author: authors[authorIndex].name,
+        authorColor: authors[authorIndex].color,
+      })
+    );
   };
 
-  const emojiInterval = setInterval(sendRandomEmoji, 2000);
+  const emojiInterval = setInterval(sendRandomEmoji, 5000);
 
   socket.on("close", () => {
     console.log("Connection closed");
